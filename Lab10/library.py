124dcfb96bb0f5e10150566b239b8f95c96fe172
Роман Ненадовьский <roman.nenadovskyi.ikh.2024@lpnu.ua>
2026-06-11 11:59:07 +0300

    Delete Lab10 directory

--- Lab10/library.py
+++
@@ -1,8 +0,0 @@
-class LibraryItem:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
-
-    def details(self):
-        return f"{self.title} - {self.author} ({self.year})"
